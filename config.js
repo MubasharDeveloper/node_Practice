e193aa59d0f71c272576b8188136e93f95922202
Mubashar Ahmad <MubasharDev0203@gmail.com>
2025-09-24 14:04:18 +0500

    Update Config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017");
+// const client = new MongoClient("mongodb://localhost:27017");
+const client = new MongoClient("mongodb+srv://mubashardev0204_db_user:1eBdJ0c7jDoupbGc@cluster0.tvy8koz.mongodb.net/");
 
 let db;
 
